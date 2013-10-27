1cd9b8076495a18edbf66ef480079d545f7f6643
Davide Dal Colle <info@davidedalcolle.it>
2013-10-27 12:58:27 +0100

    package setup

---
+++ packages/vector/package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  summary: "Vector cms",
+  environments: ['client', 'server'] // optional. Supposedly specifies environments to load package, but i did not find any package that specifies this key.
+});
+
+Package.on_use(function (api, where){
+
+  api.use('coffeescript', ['client','server']);
+  api.use('accounts-password', 'server');
+  api.use(['templating','stylus','handlebars','iron-router'], 'client');
+
+  api.export('test');
+
+  api.add_files('file_name.coffee', 'client');
+});
